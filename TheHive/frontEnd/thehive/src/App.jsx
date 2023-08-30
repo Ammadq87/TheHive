7a296b72675fa6b5937f7767a7c71be3d417597f
Ammad <ammadq87@my.yorku.ca>
2023-08-30 00:32:39 -0400

    React and tailwind additions

---
+++ TheHive/frontEnd/thehive/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p class='font-bold text-red-800'>hi</p>
+    </div>
+  )
+}
+
+export default App
