c8b570e6fb5f6ee918836b0960bdb64789d3a42e
Ammad <ammadq87@my.yorku.ca>
2023-09-02 13:17:55 -0400

    Added pages

--- TheHive/frontEnd/thehive/src/App.jsx
+++ TheHive/frontEnd/thehive/src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home';
-import Navbar from './components/Navbar'
 import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import Create from './pages/Create'
+import Spaces from './pages/Spaces'
+import Navbar from './components/Navbar';
+
 // https://dribbble.com/shots/17159089-DoWhith-Personal-Wallet-Dashboard
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="root">
+      <Navbar/>
+
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/create' element={<Create/>}/>
+          <Route path='/spaces' element={<Spaces/>}/>          
+          
           {/* <Route path='/profile' element={<Profile/>}/> */}
           {/* <Route path='/*' element={<Error/>}></Route> */}
         </Routes>
